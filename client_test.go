c115f433b9df43721f3fb3392638e4ba57d989ed
Aaron Longwell <aaron@culturefoundry.com>
2016-09-25 20:38:19 -0700

    Test cleanup.

--- client_test.go
+++ client_test.go
@@ -1,5 +1,19 @@
 package trello
 
+import (
+	"testing"
+)
+
+func TestGetWithBadURL(t *testing.T) {
+	c := testClient()
+	target := map[string]interface{}{}
+	c.BaseURL = "gopher://test"
+	err := c.Get("members", Defaults, &target)
+	if err == nil {
+		t.Fatal("Get() should fali with a bad URL")
+	}
+}
+
 func testClient() *Client {
 	c := NewClient("user", "pass")
 	return c
