62c94e3b7520fd467af8d00ef87a8d72a97f33e9
Aaron Longwell <aaron@culturefoundry.com>
2016-09-27 00:37:13 -0700

    Adds Organization{} struct and Client.GetOrganization().

---
+++ organization_test.go
@@ -0,0 +1,23 @@
+// Copyright © 2016 Aaron Longwell
+//
+// Use of this source code is governed by an MIT licese.
+// Details in the LICENSE file.
+
+package trello
+
+import (
+	"testing"
+)
+
+func TestGetOrganization(t *testing.T) {
+	client := testClient()
+	client.BaseURL = mockResponse("organizations", "culturefoundry.json").URL
+	organization, err := client.GetOrganization("culturefoundry", Defaults)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if organization.DisplayName != "Culture Foundry" {
+		t.Errorf("Expected name 'Culture Foundry'. Got '%s'.", organization.DisplayName)
+	}
+}
