e08dab56fb44995fd8214e011405b2df1a72cbe0
Aaron Longwell <aaron@culturefoundry.com>
2016-10-01 22:30:03 -0700

    Adds card.GetListChangeActions() and ListAfterAction() to assist in building the ListDuration feature.

--- action.go
+++ action.go
@@ -21,9 +21,10 @@ type Action struct {
 
 type ActionData struct {
 	Text           string    `json:"text,omitempty"`
-	Board          *Board    `json:"board,omitempty"`
 	List           *List     `json:"list,omitempty"`
 	Card           *Card     `json:"card,omitempty"`
+	Board          *Board    `json:"board,omitempty"`
+	Old            *Card     `json:"old,omitempty"`
 	ListBefore     *List     `json:"listBefore,omitempty"`
 	ListAfter      *List     `json:"listAfter,omitempty"`
 	DateLastEdited time.Time `json:"dateLastEdited"`
@@ -48,4 +49,47 @@ func (c *Card) GetActions(args Arguments) (actions []*Action, err error) {
 	path := fmt.Sprintf("cards/%s/actions", c.ID)
 	err = c.client.Get(path, args, &actions)
 	return
+}
+
+// GetListChangeActions retrieves a slice of Actions which resulted in changes
+// to the card's active List. This includes the createCard and copyCard action (which
+// place the card in its first list, and the updateCard:closed action, which remove it
+// from its last list.
+//
+// This function is just an alias for:
+//   card.GetActions(Arguments{"filter": "createCard,copyCard,updateCard:idList,updateCard:closed", "limit": "1000"})
+//
+func (c *Card) GetListChangeActions() (actions []*Action, err error) {
+	path := fmt.Sprintf("cards/%s/actions", c.ID)
+
+	args := Arguments{
+		"filter": "createCard,copyCard,updateCard:idList,updateCard:closed",
+		"limit":  "1000",
+	}
+	err = c.client.Get(path, args, &actions)
+	return
+}
+
+// ListAfterAction calculates which List the card ended up in after this action
+// completed. Returns nil when the action resulted in the card being archived (in
+// which case we consider it to not be in a list anymore), or when the action isn't
+// related to a list at all (in which case this is a nonsensical question to ask).
+//
+func ListAfterAction(a *Action) *List {
+	switch a.Type {
+	case "copyCard":
+		return a.Data.List
+	case "createCard":
+		return a.Data.List
+	case "updateCard":
+		if a.Data.Card != nil && a.Data.Card.Closed {
+			return nil
+		} else if a.Data.Old != nil && a.Data.Old.Closed {
+			return a.Data.List
+		}
+		if a.Data.ListAfter != nil {
+			return a.Data.ListAfter
+		}
+	}
+	return nil
 }
--- action_test.go
+++ action_test.go
@@ -12,7 +12,7 @@ import (
 func TestGetActionsOnBoard(t *testing.T) {
 	board := testBoard(t)
 	board.client.BaseURL = mockResponse("actions", "board-actions-api-example.json").URL
-	actions, err := board.GetActions(Defaults)
+	actions, err := board.GetActions(Defaults())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -25,7 +25,7 @@ func TestGetActionsOnBoard(t *testing.T) {
 func TestGetActionsOnList(t *testing.T) {
 	list := testList(t)
 	list.client.BaseURL = mockResponse("actions", "list-actions-api-example.json").URL
-	actions, err := list.GetActions(Defaults)
+	actions, err := list.GetActions(Defaults())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -37,11 +37,78 @@ func TestGetActionsOnList(t *testing.T) {
 func TestGetActionsOnCard(t *testing.T) {
 	card := testCard(t)
 	card.client.BaseURL = mockResponse("actions", "card-actions-api-example.json").URL
-	actions, err := card.GetActions(Defaults)
+	actions, err := card.GetActions(Defaults())
 	if err != nil {
 		t.Fatal(err)
 	}
 	if len(actions) != 50 {
 		t.Errorf("Expected 50 actions, got %d", len(actions))
+	}
+}
+
+func TestListAfterActionOnUpdateCard(t *testing.T) {
+	a := &Action{
+		Type: "updateCard",
+		Data: &ActionData{
+			ListBefore: &List{Name: "Before"},
+			ListAfter:  &List{Name: "After"},
+		},
+	}
+	l := ListAfterAction(a)
+	if l.Name != "After" {
+		t.Errorf("Incorrect List name '%s'", l.Name)
+	}
+}
+
+func TestListAfterActionOnArchive(t *testing.T) {
+	a := &Action{
+		Type: "updateCard",
+		Data: &ActionData{
+			List:  &List{Name: "SameList"},
+			Board: &Board{},
+			Card:  &Card{Closed: true},
+			Old:   &Card{Closed: false},
+		},
+	}
+	l := ListAfterAction(a)
+	if l != nil {
+		t.Error("ListAfterAction() should be nil after an archive.")
+	}
+}
+
+func TestListAfterActionOnUnarchive(t *testing.T) {
+	a := &Action{
+		Type: "updateCard",
+		Data: &ActionData{
+			List:  &List{Name: "SameList"},
+			Board: &Board{},
+			Card:  &Card{Closed: false},
+			Old:   &Card{Closed: true},
+		},
+	}
+	l := ListAfterAction(a)
+	if l == nil {
+		t.Error("ListAfterAction() should not be nil after an unarchive.")
+	}
+	if l.Name != "SameList" {
+		t.Errorf("Incorrect List name '%s'.", l.Name)
+	}
+}
+
+func TestListAfterActionOnCopyCard(t *testing.T) {
+	a := &Action{
+		Type: "copyCard",
+		Data: &ActionData{
+			List:  &List{Name: "FirstList"},
+			Board: &Board{},
+			Card:  &Card{Closed: false},
+		},
+	}
+	l := ListAfterAction(a)
+	if l == nil {
+		t.Error("ListAfterAction() should not be nil after a copy")
+	}
+	if l.Name != "FirstList" {
+		t.Errorf("Incorrect List name '%s'.", l.Name)
 	}
 }
