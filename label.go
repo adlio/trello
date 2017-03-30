f4565357a098ffd5b7052b969739a7ebe15e9e79
Aaron Longwell <aaron@culturefoundry.com>
2017-03-30 09:54:38 -0700

    Label.ID and Label.IDBoard had the same JSON struct tag.

--- label.go
+++ label.go
@@ -7,7 +7,7 @@ package trello
 
 type Label struct {
 	ID      string `json:"id"`
-	IDBoard string `json:"id"`
+	IDBoard string `json:"idBoard"`
 	Name    string `json:"name"`
 	Color   string `json:"color"`
 	Uses    int    `json:"uses"`
