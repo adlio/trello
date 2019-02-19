21795ea2ac0eefba48ede13a880ea8e4cf101a30
jtsaito <jahn.saito@gmail.com>
2019-02-19 10:12:07 +0900

    Add comments to public members of action-collection (golint)

--- action-collection.go
+++ action-collection.go
@@ -12,6 +12,7 @@ func (c ActionCollection) Len() int           { return len(c) }
 func (c ActionCollection) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
 func (c ActionCollection) Less(i, j int) bool { return c[i].ID < c[j].ID }
 
+// FirstCardCreateAction returns first card-create action
 func (actions ActionCollection) FirstCardCreateAction() *Action {
 	sort.Sort(actions)
 	for _, action := range actions {
@@ -22,10 +23,12 @@ func (actions ActionCollection) FirstCardCreateAction() *Action {
 	return nil
 }
 
+// ContainsCardCreation returns true if collection contains a card-create action
 func (actions ActionCollection) ContainsCardCreation() bool {
 	return actions.FirstCardCreateAction() != nil
 }
 
+// FilterToCardCreationActions returns this collection's card-create actions
 func (c ActionCollection) FilterToCardCreationActions() ActionCollection {
 	newSlice := make(ActionCollection, 0, len(c))
 	for _, action := range c {
@@ -36,6 +39,7 @@ func (c ActionCollection) FilterToCardCreationActions() ActionCollection {
 	return newSlice
 }
 
+// FilterToListChangeActions returns card-change-list actions
 func (c ActionCollection) FilterToListChangeActions() ActionCollection {
 	newSlice := make(ActionCollection, 0, len(c))
 	for _, action := range c {
@@ -46,6 +50,7 @@ func (c ActionCollection) FilterToListChangeActions() ActionCollection {
 	return newSlice
 }
 
+// FilterToCardMembershipChangeActions returns the collection's card-change, archive and unarchive actions
 func (c ActionCollection) FilterToCardMembershipChangeActions() ActionCollection {
 	newSlice := make(ActionCollection, 0, len(c))
 	for _, action := range c {
