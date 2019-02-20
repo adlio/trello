12e9cb230f15da4fb953d88d2ae4d82ed5333da2
jtsaito <jahn.saito@gmail.com>
2019-02-20 14:25:29 +0900

    Update label.go

--- label.go
+++ label.go
@@ -7,6 +7,9 @@ package trello
 
 import "fmt"
 
+// Label represents a Trello label.
+// Labels are defined per board, and can be applied to the cards on that board.
+// https://developers.trello.com/reference/#label-object
 type Label struct {
 	ID      string `json:"id"`
 	IDBoard string `json:"idBoard"`
@@ -15,12 +18,15 @@ type Label struct {
 	Uses    int    `json:"uses"`
 }
 
+// GetLabel takes a label id and Arguments and returns the matching label (per Trello member)
+// or an error.
 func (c *Client) GetLabel(labelID string, args Arguments) (label *Label, err error) {
 	path := fmt.Sprintf("labels/%s", labelID)
 	err = c.Get(path, args, &label)
 	return
 }
 
+// GetLabels takes Arguments and returns a slice containing all labels of the receiver board or an error.
 func (b *Board) GetLabels(args Arguments) (labels []*Label, err error) {
 	path := fmt.Sprintf("boards/%s/labels", b.ID)
 	err = b.client.Get(path, args, &labels)
