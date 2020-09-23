344237224bfe4e91c9854c7d2caa3f3cee3e59c4
Laura Martin <surminus@gmail.com>
2020-09-23 09:03:10 +0100

    Add GetMyMember function
    This is useful if you want to be able to quickly return details about
    the user making API calls.

--- member.go
+++ member.go
@@ -31,6 +31,16 @@ func (c *Client) GetMember(memberID string, args Arguments) (member *Member, err
 	return
 }
 
+// GetMyMember returns Member for the user authenticating the API call
+func (c *Client) GetMyMember(args Arguments) (member *Member, err error) {
+	path := fmt.Sprintf("members/me")
+	err = c.Get(path, args, &member)
+	if err == nil {
+		member.client = c
+	}
+	return
+}
+
 // GetMembers takes Arguments and returns a slice of all members of the organization or an error.
 func (o *Organization) GetMembers(args Arguments) (members []*Member, err error) {
 	path := fmt.Sprintf("organizations/%s/members", o.ID)
